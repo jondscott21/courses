72b72761449bfd0326590d03ca57f44a54a00039
Jonathan Scott <jonathan.scott@gmail.com>
2017-02-19 00:22:50 -0800

    django

--- coursesFeb17-master/JonScottArnoldSon/courses/apps/course/urls.py
+++ coursesFeb17-master/JonScottArnoldSon/courses/apps/course/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^process$', views.process),
     url(r'^course/destroy/(?P<id>\d+)$', views.destroy),
-    url(r'^deleted/(?P<id>\d+)$', views.deleted)
+    url(r'^deleted/(?P<id>\d+)$', views.deleted),
+    url(r'^comments$', views.comments),
+    url(r'^comments/(?P<id>\d+)$', views.comments),
+    url(r'^process2/(?P<id>\d+)$', views.process2),
 
 ]
--- coursesFeb17-master/JonScottArnoldSon/courses/apps/course/views.py
+++ coursesFeb17-master/JonScottArnoldSon/courses/apps/course/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
-from .models import Course
+from .models import Course, Description, Comment
 
 def index(request):
     context = {
@@ -8,19 +8,25 @@ def index(request):
 
     return render(request, 'course/index.html', context)
 def process(request):
-    print 'got here'
     if request.method == 'POST':
-        print 'hello'
-        print request.POST['name'], request.POST['desc']
-        Course.objects.create(course_name=request.POST['name'], description=request.POST['desc'])
+       course = Course.objects.create(course_name=request.POST['name'])
+       Description.objects.create(course=course, description=request.POST['desc'])
     return redirect('/')
 def destroy(request, id):
-    print id
     context = {
         'rem_course': Course.objects.get(id=id)
     }
     return render(request, 'course/destroy.html', context)
 def deleted(request, id):
     Course.objects.filter(id=id).delete()
-
     return redirect('/')
+def comments(request, id):
+    context = {
+        'course': Course.objects.get(id=id),
+        'comments': Comment.objects.filter(course=id),
+    }
+    return render(request, 'course/comments.html', context)
+def process2(request, id):
+    Comment.objects.create(comment=request.POST['comment'], course=Course.objects.get(id=id))
+    id = id
+    return redirect('/comments/{}'.format(id))
